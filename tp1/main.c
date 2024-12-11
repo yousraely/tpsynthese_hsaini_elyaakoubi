59da887cf76c3fe368b7f67c283406b0afe198e9
ensea <ensea@StudentLab>
2024-12-11 17:43:16 +0100

    fin tp1

--- tp1/main.c
+++ tp1/main.c
@@ -1,7 +1,30 @@
 #include "stdlib.h"
-
+#include "unistd.h"
+#include "string.h"
+#include "stdio.h"
+;
 int main(int argc, char *argv[]){
-    write(1, "Hello world\n", 12);
+    // We define our different messages as character strings.
+    char welcome[]="Bienvenue dans le shell ENSEA.\nPour quitter, tapez 'exit'.\n";
+    char prompt[]="enseash%";
+    char unknown[]="Commande inconnue .\n";
+    char bye[]="Bye Bye ;).\n";
+    char buffer[50];
+    write(1, welcome, sizeof(welcome)); 
 
-    exit(EXIT_SUCCESS);
+while(1){
+    write(1, prompt, sizeof(prompt));
+    int letter = read(0,buffer,sizeof(buffer)); // // Reads the data entered by the user and stores it in the buffer
+    if (letter<=0) exit(0); // If no word or letter is entered, the program ends with an exit
+    buffer[letter-1]='\0'; //Replace the last character with a null character (to ignore the last character in the next read)
+
+//Checks if the user typed "exit" by comparing each character with those in the string "exit"
+    if (buffer[0] == 'e' && buffer[1] == 'x' && buffer[2] == 'i' && buffer[3] == 't' && buffer[4] == '\0'){ 
+    write(1, bye, sizeof(bye));
+    exit(0); 
+    }
+
+    write(1,unknown,sizeof(unknown) );
 }
+}
+
